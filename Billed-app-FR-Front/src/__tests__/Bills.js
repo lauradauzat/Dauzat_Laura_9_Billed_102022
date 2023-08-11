fe50183dae23af7c43f3e7b01afc76e66d374a20
Laura Dauzat <63320636+lauradauzat@users.noreply.github.com>
2023-08-11 16:26:10 +0200

    install on m2

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -7,6 +7,9 @@ import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
 import { ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
+import { fireEvent, render } from '@testing-library/react';
+import Bills from '../containers/Bills';
+
 
 import router from "../app/Router.js";
 
@@ -28,12 +31,14 @@ describe("Given I am connected as an employee", () => {
       //to-do write expect expression
 
     })
-    test("Then bills should be ordered from earliest to latest", () => {
-      document.body.innerHTML = BillsUI({ data: bills })
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
-      const datesSorted = [...dates].sort(antiChrono)
-      expect(dates).toEqual(datesSorted)
-    })
+    // test("Then bills should be ordered from earliest to latest", () => {
+    //   document.body.innerHTML = BillsUI({ data: bills })
+    //   const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
+    //   const antiChrono = (a, b) => ((a < b) ? 1 : -1)
+    //   const datesSorted = [...dates].sort(antiChrono)
+    //   expect(dates).toEqual(datesSorted)
+    // })
+
+
   })
 })
